f26e7cd4f090b729caa3b321649fd8fc51b23ba1
Louis Garman <louisgarman@gmail.com>
2025-02-07 07:50:21 +0000

    wip

--- db.go
+++ db.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"strings"
 
 	sq "github.com/Masterminds/squirrel"
 	_ "modernc.org/sqlite"
@@ -15,7 +17,7 @@ type db struct {
 }
 
 func newDB() (*db, error) {
-	conn, err := sql.Open("sqlite", "file:books.sql")
+	conn, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
 		return nil, err
 	}
@@ -44,12 +46,41 @@ func (db *db) insertRow(row []string) error {
 }
 
 func (db *db) getCell(row, column int) string {
+	var sql strings.Builder
+	sql.WriteString("SELECT ")
+	sql.WriteString(fmt.Sprintf("c%d.value", column))
+	sql.WriteString(" FROM (")
+	for i := range db.columns {
+		if i > 0 {
+			sql.WriteString(" LEFT JOIN ")
+		}
+		sql.WriteString("(SELECT row, value FROM cells ")
+		sql.WriteString(fmt.Sprintf("WHERE column = %d", i))
+		sql.WriteString(")")
+		sql.WriteString(" AS ")
+		sql.WriteString(fmt.Sprintf("c%d", i))
+		if i > 0 {
+			sql.WriteString(" USING (row)")
+		}
+	}
+	sql.WriteString(" )")
+	sql.WriteString(" ORDER BY ")
+	for i := range db.columns {
+		if i > 0 {
+			sql.WriteString(", ")
+		}
+		sql.WriteString(fmt.Sprintf("c%d.value", i))
+	}
+	sql.WriteString(" LIMIT 1 OFFSET ")
+	sql.WriteString(fmt.Sprintf("%d", row))
+	// log.Println(sql.String())
 	var value string
-	err := db.conn.QueryRow("SELECT value FROM cells WHERE column = ? ORDER BY (value AND column = 0) LIMIT 1 OFFSET ?", column, row).Scan(&value)
+	err := db.conn.QueryRow(sql.String()).Scan(&value)
 	if err != nil {
-		log.Fatal("At(): ", err.Error())
+		log.Fatal("getCell(): ", err.Error())
 	}
 	return value
+	// sqlite> select c1.value as "0", c2.value as "1" from (select row, value from cells where column = 0) as c1 join (select row, value from cells where column = 1) as c2 using (row) order by 1, 2;
 }
 
 func (db *db) getRowCount() int {
